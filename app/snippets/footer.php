0fbb2819092918ee2f01ca79887926b6064d89ab
Adam Paszternak <hello@paszternak.me>
2017-08-15 08:09:41 +0200

    Move logout to footer

--- app/snippets/footer.php
+++ app/snippets/footer.php
@@ -1,6 +1,9 @@
 <div class="clear"></div>
 <hr class="footer" />
 <div class="help-link">
+	<span class="logout-link">
+		<a href="logout"><i class="fa fa-sign-out" aria-hidden="true"></i> <span><?php echo $lang['MENU_LOGOUT']; ?></span></a>
+	</span>
 	<a href="help"><i class="fa fa-question-circle" aria-hidden="true"></i> <?php echo $lang['HELP_TITLE']; ?></a><span
 		class="dlmtr">&emsp;</span> <a href="random"><i class="fa fa-random"
 		aria-hidden="true"></i> <?php echo $lang['RANDOM_TITLE']; ?></a>
